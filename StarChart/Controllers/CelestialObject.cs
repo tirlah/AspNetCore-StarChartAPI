4037efc697eed4b2d53c87a36c3ca5d70051d2b2
Mikhail Kutyrev <kutyrevma@volgatech.net>
2021-09-24 19:15:31 +0300

    3

---
+++ StarChart/Controllers/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase 
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        
+    }
+}
